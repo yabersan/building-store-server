74571cadc9adf636f2e158e2f3fcb53fa0cc7d94
Bers <semeikayaskaevih@gmail.com>
2023-01-25 20:38:27 +0500

    добавил маршруты в роуты в index.js. изменил роуты в reviews.route.js. добавил маршрут в products.route.js. изменил название ключа addNews. изменил название константы imageURL на productePicture

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.use("/users", require("./users.route"));
+router.use("/products", require("./products.route"));
+router.use("/reviews", require("./reviews.route"));
 
 module.exports = router;
