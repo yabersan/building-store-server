426dfc14fff4c733f447d350fdc2680da25d5fc9
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2023-01-25 23:46:39 +0300

    Добавил возможность отправить сообщения на почту.

--- routes/products.route.js
+++ routes/products.route.js
@@ -4,5 +4,8 @@ const { productsController } = require("../controllers/products.controller");
 const image = require("../middlewares/file.middleware");
 
 router.post("/", image.single("image"), productsController.addProduct);
+router.post("/email", productsController.sendEmail);
+
+
 
 module.exports = router;
