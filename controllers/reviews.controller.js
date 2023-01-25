03dfe345a91db1896cf79fbfb41cc455a7e9ecc2
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2023-01-25 15:27:38 +0300

    Добавил роуты для отзывов, и дописал рес.джсоны в контроллере отзывов.

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -1,32 +1,31 @@
 const Review = require("../models/Review.model");
 
 module.exports.reviewControllers = {
-    addReview: async (req, res) => {
-        const {text} = req.body;
-        const userId = req.user
-        try {
-            await Review.create({text, userId})
-            return res.json("Отзыв добавлен.")
-        } catch (error) {
-            return res.json(error + "Ошибка при добавлении отзыва.")
-        }
-    },
-    deleteReview: async (req, res) => {
-        const {id} = req.body
-        try{
-            await Review.findByIdAndRemove(id)
-            return res.json("Отзыв удален.")
-        }catch(error){
-            return res.json(error + "Ошибка при удалении отзыва.")
-        }
-    },
-    getReviews: async (req, res) => {
-        try{
-            const reviews = await Review.find()
-            return res.json()
-        }catch(error){
-
-        }
+  addReview: async (req, res) => {
+    const { text } = req.body;
+    const userId = req.user;
+    try {
+      await Review.create({ text, userId });
+      return res.json("Отзыв добавлен.");
+    } catch (error) {
+      return res.json(error + "Ошибка при добавлении отзыва.");
     }
-
-}
+  },
+  deleteReview: async (req, res) => {
+    const { id } = req.body;
+    try {
+      await Review.findByIdAndRemove(id);
+      return res.json("Отзыв удален.");
+    } catch (error) {
+      return res.json(error + "Ошибка при удалении отзыва.");
+    }
+  },
+  getReviews: async (req, res) => {
+    try {
+      const reviews = await Review.find();
+      return res.json(reviews);
+    } catch (error) {
+      return res.json(error + "Ошибка при получении отзывов");
+    }
+  },
+};
