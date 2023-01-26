b0c874dbf2cd5ff6f8f455b4bfdef35c7a2b1c76
Bers <semeikayaskaevih@gmail.com>
2023-01-26 17:53:42 +0500

    добавил котроллер для автозавершения

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -20,9 +20,33 @@ module.exports.productsController = {
   getProducts: async (req, res) => {
     try {
       const products = await Product.find();
+      console.log(products);
       return res.json(products);
     } catch (error) {
       res.json({ error: error.message });
+    }
+  },
+  getProducts: async (req, res) => {
+    try {
+      const products = await Product.find();
+      console.log(products);
+      return res.json(products);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  },
+  autocompleteProducts: async (req, res) => {
+    const searchProduct = req.body;
+    try {
+      const products = await Product.find({
+        title: new RegExp(`${searchProduct}`, "i"),
+      });
+      if (news.length !== 0) {
+        return res.json(products);
+      }
+      return res.json({ error: "По вашему запросу ничего не найдено..." });
+    } catch (error) {
+      return res.json(error);
     }
   },
 };
