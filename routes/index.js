efd4ec06ef35c9c8faa0cafd08fa8b9a1025937a
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2023-01-27 21:49:01 +0300

    Добавил чат

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ const router = Router();
 router.use("/users", require("./users.route"));
 router.use("/products", require("./products.route"));
 router.use("/reviews", require("./reviews.route"));
+router.use("/chats", require("./chats.route"));
+
 
 module.exports = router;
